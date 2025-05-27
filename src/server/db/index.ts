2458bc88e6db696d5264a03aacadb5338728348f
jan <ivan.stsepaniuk@gmail.com>
2025-05-27 13:44:12 +0200

    Update database connection environment variable

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -6,6 +6,6 @@ import * as schema from "./schema";
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.STORAGE_DATABASE_URL!);
 
 export const db = drizzle(sql, { schema });
