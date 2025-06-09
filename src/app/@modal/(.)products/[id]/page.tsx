59479fcb597bf4dcdad808654ce64fb841a1035e
jan <ivan.stsepaniuk@gmail.com>
2025-06-09 19:36:22 +0200

    Implement size options and update global styles
    Added size option functionality to the product modal, allowing dynamic price adjustments based on selection. Extended global styles with additional themes, dark mode variants, and Tailwind animations. Updated dependencies to include Radix UI components, `clsx`, `tailwind-merge`, and related packages.

--- src/app/@modal/(.)products/[id]/page.tsx
+++ src/app/@modal/(.)products/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use server"
 import { Modal } from "./modal";
-import { getProductsByID } from "~/server/queries";
+import { getOptionsForProduct, getProductsByID } from "~/server/queries";
 import ProductModal from "~/components/modalProduct";
 
 export default async function PhotoModal(
@@ -10,8 +10,11 @@ export default async function PhotoModal(
   params: Promise<{ id: number }>;
 }) {
   const photoId = (await params).id;
-  const product = await getProductsByID(photoId);
+  const [product, sizeOptions] = await Promise.all([
+    getProductsByID(photoId),
+    getOptionsForProduct({ groupName: "Size", productId: photoId })
+  ]);
   if (!product) return <Modal><div>Product not found</div></Modal>;
 
-  return <Modal><ProductModal product={product}/></Modal>
+  return <Modal><ProductModal product={product} sizeOptions={sizeOptions}/></Modal>
 }
--- src/components/modalProduct.tsx
+++ src/components/modalProduct.tsx
@@ -1,6 +1,8 @@
 "use client"
 import Image from "next/image";
-import { useState } from "react";
+import React, { useState } from "react";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group"
+import { Label } from "./ui/label"
 
 interface Product {
   id: number;
@@ -11,9 +13,20 @@ interface Product {
   updatedAt: Date | null;
 
 }
+interface SizeOption {
+  productId: number;
+  groupName: string;
+  optionId: number;
+  value: string;
+  priceMultiplier: number | null;
+}
 
-export default function ProductModal({ product }: { product: Product }) {
+const ProductModal = React.memo(function ProductModal({ product, sizeOptions }: { product: Product, sizeOptions: SizeOption[] }) {
   const [finalPrice, setFinalPrice] = useState(product.price)
+  console.log(sizeOptions)
+  const changePrice = (multiplier:number) => {
+    setFinalPrice(product.price + multiplier )
+  }
   return (
     <div className="w-full flex flex-col gap-4 text-lg">
       <h1 className="text-2xl font-semibold text-center mb-2">Specify your order</h1>
@@ -27,8 +40,20 @@ export default function ProductModal({ product }: { product: Product }) {
           />
         </div>
         <div className="flex flex-col gap-4 justify-between p-2">
-          <div className="font-medium">{product.nameProduct} <span className="text-gray-600">{product.price} pln</span></div>
-
+          <div className="font-medium">{product.nameProduct} <span className="text-gray-600">{product.price} pln</span>
+          </div>
+          {sizeOptions.length > 0 && (
+            <RadioGroup
+            onValueChange={(value) => changePrice(sizeOptions.find((option) => option.value === value)?.priceMultiplier ?? 0)}
+            >
+              {sizeOptions.map((option, index) => (
+                <div key={index} className="flex items-center space-x-2">
+                  <RadioGroupItem value={option.value} id={option.value} />
+                  <Label htmlFor={option.value}>{option.value}</Label>
+                </div>
+              ))}
+            </RadioGroup>
+          )}
           <div className="flex flex-col gap-2">
             <span className="font-bold text-xl">{finalPrice} pln</span>
             <button className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-colors">
@@ -39,4 +64,6 @@ export default function ProductModal({ product }: { product: Product }) {
       </div>
     </div>
   );
-}
+});
+
+export default ProductModal;
